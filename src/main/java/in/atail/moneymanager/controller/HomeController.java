3db2de5d4293997edf8bc87fc2cfba3efd225ad5
Atail <17382579293@163.com>
2026-03-13 11:33:21 +0800

    完成初始化

---
+++ src/main/java/in/atail/moneymanager/controller/HomeController.java
@@ -0,0 +1,15 @@
+package in.atail.moneymanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/health","status"})
+public class HomeController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "程序正在运行";
+    }
+}
